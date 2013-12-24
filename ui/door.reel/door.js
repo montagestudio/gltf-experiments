58036b5792b2d890e81e702064206e26fe817f8f
Fabrice Robinet <fabricerobinet@me.com>
2013-12-23 21:46:35 -0800

    adding Beach Planet model and 3 examples

---
+++ ui/door.reel/door.js
@@ -0,0 +1,17 @@
+var Component;
+
+Component = require("montage/ui/component").Component;
+
+exports.Door = Component.specialize({
+
+  handleDoorAction: {
+    value: function(event) {
+    	if (!this.templateObjects.door.classList.has(".open")) {
+	   		this.templateObjects.door.classList.add(".open");
+    	} else {
+	   		this.templateObjects.door.classList.remove(".open");
+    	}
+    }
+  }
+
+});
