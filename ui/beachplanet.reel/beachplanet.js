9bfbc6e01df26186c07bfd363d90b90fbf2556f3
Fabrice Robinet <fabricerobinet@me.com>
2014-01-02 21:11:16 -0800

    reveal logo under the rock and a few minor fixes, not happy that we don't have ways to properly make a 3d translate nor transform origin with 3 components...

--- ui/beachplanet.reel/beachplanet.js
+++ ui/beachplanet.reel/beachplanet.js
@@ -4,6 +4,10 @@ Component = require("montage/ui/component").Component;
 
 exports.BeachPlanet = Component.specialize({
 
+	playing: { value: false },
+
+	score: { value: 0 },
+
 	templateDidLoad: {
 		value: function () {
 			this.templateObjects.viewer.play();
@@ -12,8 +16,19 @@ exports.BeachPlanet = Component.specialize({
 
 	handleNavItemAction: { 
 		value: function(event) {
+			this.playing = true;
 			this.templateObjects.viewer.stop();
 			this.templateObjects.viewer.viewPoint = event.target.viewPoint;
+		}
+	},
+
+	handleRockHover: {
+		value: function(event) {
+	    	if (this.playing && !this.templateObjects.rock.classList.has(".BeachPlanet-rock-reveal")) {
+	   			this.templateObjects.rock.classList.add(".BeachPlanet-rock-reveal");
+	   			this.templateObjects.logoRock.classList.add(".BeachPlanet-rock-logo-reveal");
+	   			this.score++;
+			}
 		}
 	}
 
