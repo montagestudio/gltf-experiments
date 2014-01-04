a682ba6c1b1e3bb1fa9790fe5c4b7f3322239783
Fabrice Robinet <fabricerobinet@me.com>
2014-01-03 16:33:37 -0800

    open door and look at when doing so

--- ui/beachplanet.reel/beachplanet.js
+++ ui/beachplanet.reel/beachplanet.js
@@ -22,7 +22,9 @@ exports.BeachPlanet = Component.specialize({
 		}
 	},
 
-	handleRockHover: {
+	/* handle rock */
+
+	handleRockAction: {
 		value: function(event) {
 	    	if (this.playing && !this.templateObjects.rock.classList.has(".BeachPlanet-rock-reveal")) {
 	   			this.templateObjects.rock.classList.add(".BeachPlanet-rock-reveal");
@@ -30,6 +32,36 @@ exports.BeachPlanet = Component.specialize({
 	   			this.score++;
 			}
 		}
+	},
+
+	handleRockHover: {
+		value: function(event) {
+        	if (this.playing && !this.templateObjects.rock.classList.has(".BeachPlanet-rock-reveal")) {
+				this.templateObjects.viewer.element.style.cursor="pointer";   			
+	    	}
+		}
+	},
+
+	/* handle door */
+
+  	handleDoorAction: {
+    	value: function(event) {
+    		if (this.playing && !this.templateObjects.door.classList.has(".BeachPlanet-door-open")) {
+	   			this.templateObjects.viewer.viewPoint = this.templateObjects.cabinVP;
+	   			console.log("handleDoorAction");
+	   			this.templateObjects.door.classList.add(".BeachPlanet-door-open");
+	   			this.score++;
+    		} 
+    	}
+    },
+
+	handleDoorHover: {
+		value: function(event) {
+        	if (this.playing && !this.templateObjects.rock.classList.has(".BeachPlanet-door-open")) {
+				this.templateObjects.viewer.element.style.cursor="pointer";   			
+	    	}
+		}
 	}
+
 
 });
