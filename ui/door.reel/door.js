6d7275dad307afb72cc441d68c3d41a119a5c578
Fabrice Robinet <fabricerobinet@me.com>
2014-01-16 13:17:30 -0800

    add fallback for door

--- ui/door.reel/door.js
+++ ui/door.reel/door.js
@@ -4,14 +4,48 @@ Component = require("montage/ui/component").Component;
 
 exports.Door = Component.specialize({
 
-  doorOpen: {
-    value: false
-  },
+  	doorOpen: {
+    	value: false
+  	},
 
-  handleDoorAction: {
-    value: function(event) {
-      this.doorOpen = ~this.doorOpen;
+  	handleDoorAction: {
+    	value: function(event) {
+      		this.doorOpen = ~this.doorOpen;
+    	}
+  	},
+
+  	_supportsWebGL: {
+        value: null
+    },
+
+    /**
+     * Whether or not the current user agent supports WebGL
+     */
+    supportsWebGL: {
+        get: function () {
+
+            if (null === this._supportsWebGL) {
+                var webGLOptions = {premultipliedAlpha: false, antialias: true, preserveDrawingBuffer: false};
+                var canvas = document.createElement("canvas");
+                var webGLContext =  canvas.getContext("experimental-webgl", webGLOptions) ||
+                    canvas.getContext("webgl", webGLOptions);
+                this._supportsWebGL = !!webGLContext;
+            }
+
+            return this._supportsWebGL;
+        }
+    },
+
+    viewKey: {
+        value: null
+    },
+
+    templateDidLoad: {
+        value: function() {
+            //Set initial view to be webgl if possible
+            this.viewKey = this.supportsWebGL ? "webgl" : "static";
+        }
     }
-  }
+
 
 });
