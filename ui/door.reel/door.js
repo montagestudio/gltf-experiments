42a5fbc8f8483e83ffa9587298bdabd64cf9bf73
Fabrice Robinet <fabricerobinet@me.com>
2014-01-16 12:21:25 -0800

    fix door example per Ryan recommendation

--- ui/door.reel/door.js
+++ ui/door.reel/door.js
@@ -4,13 +4,13 @@ Component = require("montage/ui/component").Component;
 
 exports.Door = Component.specialize({
 
+  doorOpen: {
+    value: false
+  },
+
   handleDoorAction: {
     value: function(event) {
-    	if (!this.templateObjects.door.classList.has("open")) {
-	   		this.templateObjects.door.classList.add("open");
-    	} else {
-	   		this.templateObjects.door.classList.remove("open");
-    	}
+      this.doorOpen = ~this.doorOpen;
     }
   }
 
